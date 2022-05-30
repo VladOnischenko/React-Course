df71305cbd39ab443d9e164adfe38157fdefd864
Vlad <vladonish@gmail.com>
2022-05-30 12:11:01 +0300

    test2

--- react-facts/src/App.js
+++ react-facts/src/App.js
@@ -3,7 +3,19 @@ import React from "react";
 function App() {
   return (
     <>
-      <h1>TEXT</h1>
+      <header>
+        <img src="./icon.svg" alt="react-icon" width="40px"/>
+      </header>
+      <section>
+          <h1>Fun facts about React</h1>
+          <ul>
+            <li>Was first released in 2013</li>
+            <li>Was originally created by Jordan Walke</li>
+            <li>Has well over 100K stars on GitHub</li>
+            <li>Is maintained by Facebook</li>
+            <li>Powers thousands of enterprise apps, including mobile apps</li>
+          </ul>
+      </section>
     </>
   );
 }
