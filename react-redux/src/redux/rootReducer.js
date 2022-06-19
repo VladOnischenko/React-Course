b03120bcdb3ebcf3616ece271b2f8ab7952aee89
Vlad <vladonish@gmail.com>
2022-06-19 22:18:26 +0300

    Delete mistake

--- react-redux/src/redux/rootReducer.js
+++ react-redux/src/redux/rootReducer.js
@@ -2,9 +2,12 @@ import { combineReducers } from "redux";
 import { likesReducer } from './likesReduser'
 import {inputReducer} from "./inputReduser";
 import {commentsReducer} from "./commentsReducer";
+import {commentDelete, commentUpdate} from "./actions";
 
 export const rootReducer = combineReducers({
     likesReducer,
     inputReducer,
     commentsReducer,
+    commentUpdate,
+    commentDelete,
 })
