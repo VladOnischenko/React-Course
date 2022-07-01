bf2899ea4611a38bd87d18f528c9a1adeb1e7c22
Vlad <vladonish@gmail.com>
2022-07-01 23:34:54 +0300

    Small form

--- react-redux-forms/src/App.js
+++ react-redux-forms/src/App.js
@@ -1,10 +1,20 @@
 import React from 'react'
+import './App.css'
+import skyImage from './assets/stars-image.jpeg'
+import Signup from './components/Signup'
 
 function App() {
   return (
-   <>
-
-   </>
+   <div className="container mt-3">
+      <div className="row">
+        <div className="col-md-5">
+          <Signup />
+        </div>
+        <div className="col-md-7 my-auto">
+          <img className="img-fluid w-100" src={skyImage} alt="picture"/>
+        </div>
+      </div>
+   </div>
   );
 }
 
