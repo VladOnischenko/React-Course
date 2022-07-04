fd34fdd3569de76958953d50b50cecdfdfbd29cd
Vlad <vladonish@gmail.com>
2022-07-04 11:28:29 +0300

    Error Compomemt

--- react-forms-tutorial/src/components/YoutubeForm.js
+++ react-forms-tutorial/src/components/YoutubeForm.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Formik, Form, Field, ErrorMessage } from "formik";
 import * as Yup from 'yup';
+import TextError from "./TextError";
 
 const initialValues = {
   name: '',
@@ -15,7 +16,7 @@ const validationSchema = Yup.object({
   email: Yup.string().email('Invalid email').required('Email is required'),
   channel: Yup.string().required('Channel is required'),
   comments: Yup.string(),
-  address: Yup.string(),
+  address: Yup.string().required('Address is required'),
 })
 
 const onSubmit = values => console.log(values)
@@ -31,35 +32,42 @@ const YoutubeForm = () => {
         <div className="form-control">
           <label htmlFor="name">Name</label>
           <Field type="text" id="name" name="name"/>
-          <ErrorMessage name="name"/>
+          <ErrorMessage name="name" component={TextError}/>
         </div>
 
         <div className="form-control">
           <label htmlFor="email">E-mail</label>
           <Field type="email" id="email" name="email"/>
-          <ErrorMessage name="email" />
+          <ErrorMessage name="email" component={TextError}/>
         </div>
 
         <div className="form-control">
           <label htmlFor="channel">Channel</label>
           <Field type="text" id="channel" name="channel"/>
-          <ErrorMessage name="channel"/>
+          <ErrorMessage name="channel" component={TextError}/>
         </div>
 
         <div className="form-control">
           <label htmlFor="comments">Comments</label>
           <Field as="textarea" id="comments" name="comments"/>
-          <ErrorMessage name="comments"/>
+          <ErrorMessage name="comments" component={TextError}/>
         </div>
 
         <div className="form-control">
           <label htmlFor="address">Address</label>
           <Field name="address">
             {
-              (props) => console.log(props)
+              (props) => {
+                const {field, form, meta} = props
+                return (
+                  <div>
+                    <input type="text" id="address" {...field}/>
+                    {meta.touched && meta.error ? <div className="error">{meta.error}</div> : null}
+                  </div>
+                )
+              }
             }
           </Field>
-          <ErrorMessage  name="address"/>
         </div>
 
         <button type="submit">Submit</button>
